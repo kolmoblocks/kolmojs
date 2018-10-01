497f53b5c082ee2f930eb0033f41ac8e52c991c1
Dmitry Borzov <d@borzov.ca>
2018-10-01 11:49:02 -0400

    Scaffold properties-panel rendering

--- prismo/netvis.js
+++ prismo/netvis.js
@@ -3,14 +3,15 @@
 // constructor for NetVis class
 function NetVis(DOMelement) {
 	var self = this;
-	self.DOMelement = DOMelement || "#chart";
+	self._topologyPanel = DOMelement || "#chart";
 	self.config = {
 		nodeDefaultDistance: 30,
 		nodeDefaultRadius: 10
 	};
 
 	self.Nodes = new NetVisModelNodes();
-	this.View = new NetVisView();
+	self.View = new NetVisView();
+	self._selected = self; // _selected object's public attributes are shown at properties-table
 
 	this.jsonAdapter = function(srcJSON) {
 		// jsonAdapter loads JSON in NetVis format   
@@ -24,13 +25,41 @@ function NetVis(DOMelement) {
 		}
 
 	};
-
 	this.Render = function() {
-		nodes = d3.select(self.DOMelement).append("svg").append("circle");
+		canvas = d3.select(self._topologyPanel) .append("svg")
+			.attr("width",$(self._topologyPanel).width())
+			.attr("height",$(self._topologyPanel).height());
+
+		canvas.append("circle")
+			.attr("cx", 0.5*$(self._topologyPanel).width())
+			.attr("cy", 0.5*$(self._topologyPanel).width())
+			.attr("r", 0.3*$(self._topologyPanel).width())
+			.attr("class", "contour");
+
+		nodes = canvas.selectAll("circle.node").data(self.Nodes._nodesArray).enter().append("circle");
 		nodes
-		    .attr("cx", self.config.nodeDefaultDistance)
-		    .attr("cy", self.config.nodeDefaultDistance)
+			.attr('class','node')
+		    .attr("cx", function(d) { return d.x*$(self._topologyPanel).width();})
+		    .attr("cy", function(d) { return d.y*$(self._topologyPanel).width();})
 		    .attr("r",self.config.nodeDefaultRadius);
+
+		for (var key in self._selected) {
+		   if (!self._selected.hasOwnProperty(key)) {
+		   	// inherited attribute, ignoring
+		   	continue;
+		   }
+		   if (typeof(self._selected[key]) == "function") {
+		   	// attribute is a function, ignoring
+		   	continue;
+		   }
+		   if (key.charAt(0) === "_") {
+		   	// private attribute, ignoring
+		   	continue;
+		   }
+
+	       $("#properties-tbody").append("<tr><td>"+key + "</td><td>" + self._selected[key] + "</td></tr>");
+		   
+		}
 	};
 }
 
