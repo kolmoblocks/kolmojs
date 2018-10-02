f493a68fdf67f68fe5491192575fd62997fe0c81
Dmitry Borzov <d@borzov.ca>
2018-10-02 11:51:41 -0400

    Highlight selected node on canvas

--- prismo/netvis.js
+++ prismo/netvis.js
@@ -26,7 +26,9 @@ function NetVis(DOMelement) {
 
 	};
 	this.Render = function() {
-		canvas = d3.select(self._topologyPanel) .append("svg")
+		$(self._topologyPanel).empty();
+		canvas = d3.select(self._topologyPanel) 
+			.append("svg")
 			.attr("width",$(self._topologyPanel).width())
 			.attr("height",$(self._topologyPanel).height());
 
@@ -39,10 +41,18 @@ function NetVis(DOMelement) {
 		nodes = canvas.selectAll("circle.node").data(self.Nodes._nodesArray).enter().append("circle");
 		nodes
 			.attr('class','node')
-		    .attr("cx", function(d) { return d.x*$(self._topologyPanel).width();})
-		    .attr("cy", function(d) { return d.y*$(self._topologyPanel).width();})
+		    .attr("cx", function(d) { return d._x*$(self._topologyPanel).width();})
+		    .attr("cy", function(d) { return d._y*$(self._topologyPanel).width();})
+		    .on("click",function(d) { self._selected = d; self.Render();})
 		    .attr("r",self.config.nodeDefaultRadius);
 
+		// highlight selected node
+		nodes.filter(function(d) {return self._selected.id === d.id;})
+			.attr('class','node selected')
+			.attr("r",2*self.config.nodeDefaultRadius);
+
+		$("#properties-tbody").empty();
+		attributes = [];
 		for (var key in self._selected) {
 		   if (!self._selected.hasOwnProperty(key)) {
 		   	// inherited attribute, ignoring
@@ -55,11 +65,26 @@ function NetVis(DOMelement) {
 		   if (key.charAt(0) === "_") {
 		   	// private attribute, ignoring
 		   	continue;
-		   }
-
-	       $("#properties-tbody").append("<tr><td>"+key + "</td><td>" + self._selected[key] + "</td></tr>");
-		   
+		   }		   
+		   attributes.push({"attr": key, "value":self._selected[key], "obj": typeof(self._selected[key]) == "object"});
 		}
+
+        rows = d3.select("#properties-tbody").selectAll("tr").data(attributes).enter().append("tr");
+
+        rows.append("td").text(function(d) {return d.attr; });
+
+        rows.filter(function(d) {return !d.obj;})
+	        .append("td")
+	        .append("div")
+	        .attr("class","properties-column")
+	        .text(function(d) {return d.value; });
+
+        rows.filter(function(d) {return d.obj;}).append("td").append("a").text(function(d) {return "more.."; })
+	        .on("click", function(d) {self._selected = d.value; self.Render();});
+
+
+        // append("<tr><td>"+key + "</td><td>" + self._selected[key] + "</td></tr>");
+
 	};
 }
 
