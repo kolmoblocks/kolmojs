1042f4aaeefbe0b8e31271796a3770d4b9ddb198
Dmitry Borzov <d@borzov.ca>
2018-10-10 11:59:48 -0400

    Fix asObject -> _asObject bug

--- prismo/model/history.js
+++ prismo/model/history.js
@@ -3,4 +3,23 @@
 
 NetVisHistory = function() {
 	BaseNetVisModel.apply(this); // History class inherits from baseModel
+	
+	this.loadEvent = function(obj, momentTime) {
+		obj._t = momentTime;
+		obj.time = momentTime.toISOString();
+		// eventID to be unique and contain timestamp
+		// of form "<timestamp>#3" where 3 is count of events with
+		// the same timestamp
+		var i=1,
+			obj.id = obj.time;
+		while(this._asObject[obj.id]){
+			i++;
+			obj.id = obj.time + "#" + i;
+		}
+		obj.id = obj.time;
+		this.load(obj);
+	};
+	// add default time margin moments
+	this.loadEvent({"tag":"start"},moment("1970-01-01"));	
+	this.loadEvent({"tag":"end"},moment("3000-01-01"));	
 };
