dd853ed581e2eac577d16a91c59d47b4460ee899
Dmitry Borzov <d@borzov.ca>
2018-10-11 12:00:57 -0400

    Insert events into history.asArray

--- prismo/model/history.js
+++ prismo/model/history.js
@@ -10,16 +10,38 @@ NetVisHistory = function() {
 		// eventID to be unique and contain timestamp
 		// of form "<timestamp>#3" where 3 is count of events with
 		// the same timestamp
-		var i=1,
-			obj.id = obj.time;
+		var i=1;
+		obj.id = obj.time;
 		while(this._asObject[obj.id]){
 			i++;
 			obj.id = obj.time + "#" + i;
 		}
 		obj.id = obj.time;
-		this.load(obj);
+		this._asObject[obj.id] = obj;
+
+
+		// insert momentTime so that asObject is sorted
+		// with binary search for appropriate position
+		var cur = 0,
+			lowI = 0,
+			highI = this.asArray.length;
+		while (lowI < highI) {
+			cur = Math.floor((highI + lowI) /2);
+			if (this.asArray[cur]._t.isBefore(obj._t)) {
+				lowI = cur + 1;	
+			} else {
+				highI = cur;
+			}
+		}
+		this.asArray.splice(Math.floor((highI + lowI) /2), 0,obj);
+		console.log("inserting ", obj.id, " at ", Math.floor((highI + lowI) /2));
 	};
 	// add default time margin moments
-	this.loadEvent({"tag":"start"},moment("1970-01-01"));	
 	this.loadEvent({"tag":"end"},moment("3000-01-01"));	
+	this.loadEvent({"tag":"start"},moment("1970-01-01"));	
+	this.loadEvent({"tag":"middle"},moment("2014-01-01"));	
+
+
+
+
 };
