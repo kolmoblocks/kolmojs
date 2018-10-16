8c6acb04b5db54143c71afc691f01e617a8a6a17
Dmitry Borzov <d@borzov.ca>
2018-10-16 12:07:36 -0400

    Sync time interval.message fields with messageReceived events

--- src/model/interval.js
+++ src/model/interval.js
@@ -2,11 +2,32 @@
 
 
 
-NetVisInterval = function(startEvents, endEvents) {
+NetVisInterval = function(startEvents, endEvents, prevInterval) {
 	this.startEvents = startEvents;
 	this.endEvents = endEvents;
 	this._starts = startEvents[0]._t; // as momentjs object
 	this._ends = endEvents[0]._t; // as momentjs object
 	this.starts = this._starts.toISOString(); // as ISO timestamp
 	this.ends = this._ends.toISOString();
+
+	if (prevInterval) {
+		this.messages = prevInterval.messages.slice(0); 	
+	} else {
+		this.messages = [];
+	}
+
+	for(var i=0; i< this.startEvents.length; i++) {
+		var event = this.startEvents[i]; 
+		switch (event.event) {
+			case "messageSent":
+				this.messages.push(event.message);
+				break;
+			case "messageReceived":
+				for(var j = this.messages.length - 1; j >= 0; j--) {
+				    if(this.messages[j].id === event.message.id) {
+				       this.messages.splice(j, 1);
+				    }
+				}				
+		}
+	}
 };
