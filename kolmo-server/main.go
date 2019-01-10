8ee4f66eb990c078c6b0827edef20ce4b2c9f344
William Tang <williamjt1998@gmail.com>
2019-01-10 18:21:36 -0500

    Added /raw/ endpoint and changed http response header

--- kolmo-server/main.go
+++ kolmo-server/main.go
@@ -20,10 +20,11 @@ func check(e error) {
 
 
 type Recipe struct {
-	TargetId string `json:"target_id"`
-	TargetSize int `json:"target_size"`
-	TargetTag string `json:"target_tag"`
-	TokenSize int `json:"token_size"`
+	Mime 	 	string 	`json:"MIME"`
+	TargetId 	string 	`json:"target_id"`
+	TargetSize 	int 	`json:"target_size"`
+	TargetTag 	string 	`json:"target_tag"`
+	TokenSize 	int 	`json:"token_size"`
 }
 
 
@@ -31,27 +32,80 @@ func displayJson(w http.ResponseWriter, r *http.Request) {
 	var filepath string
 	var rec Recipe
 	var data []byte
+	validSearchParams := map[string]bool{"cid": true, "target_size"	: true,
+	"target_id": true, "token_size": true,"target_tag": true}
+
 	r.ParseForm()
+
+	for  searchParam, _ := range r.Form {
+		if _, valid := validSearchParams[searchParam]; !valid {
+			fmt.Fprint(w, "Invalid search parameter:" + searchParam)
+			return
+		}
+	} 
+
 	cid := r.FormValue("cid")
 	target_size := r.FormValue("target_size")
 	target_tag := r.FormValue("target_tag")
 	target_id := r.FormValue("target_id")
 	token_size := r.FormValue("token_size")
-	folderpath := "../../runtime/out/public/" 
+	folderpath := "../../runtime/out/public/" //path to be updated 
 	files, err := ioutil.ReadDir(folderpath)
 	check(err)
+	
 	for _, f := range files {
 		filepath = folderpath + f.Name() 
 		data, err = ioutil.ReadFile(filepath)
 		check(err)
 		json.Unmarshal([]byte(data), &rec)
-		if (cid == "" || strings.HasPrefix(strings.TrimRight(f.Name(), ".json"), cid)) && 
-		(target_tag == "" || strings.HasPrefix(rec.TargetTag, target_tag)) &&
-		(target_id == "" || strings.HasPrefix(rec.TargetId, target_id)) &&
-		(target_size == "" || strconv.Itoa(rec.TargetSize) == target_size) &&
-		(token_size == "" ||  strconv.Itoa(rec.TokenSize) == token_size)  {
+		if strings.HasPrefix(strings.TrimRight(f.Name(), ".json"), cid) && 
+			strings.HasPrefix(rec.TargetTag, target_tag) &&
+			strings.HasPrefix(rec.TargetId, target_id) &&
+			(target_size == "" || strconv.Itoa(rec.TargetSize) == target_size) &&
+			(token_size == "" ||  strconv.Itoa(rec.TokenSize) == token_size)  {
+			fmt.Fprint(w, rec.TokenSize)
 			fmt.Fprint(w, string(data))
 		}
+		rec = Recipe{}
+	
+	}
+}
+
+
+// func pageNotFound (w, http.ResponseWriter, r *http.Request){
+
+// }
+
+
+func serveRawFile(w http.ResponseWriter, r *http.Request) {
+	var data []byte
+	var mime string
+	var rec Recipe
+	var filePath string
+	var metaDataPath string
+
+	vars := mux.Vars(r)
+	publicPath := "../../runtime/out/public/" //path to be updated
+	rawPath := "../../runtime/out/raw/" //path to be updated 
+
+	//Finds MIME from corresponding metadata file with target_id = cid 
+	files, err := ioutil.ReadDir(publicPath)
+	for _, f := range files {
+		metaDataPath = publicPath + f.Name() 
+		data, err = ioutil.ReadFile(metaDataPath)
+		check(err)
+		json.Unmarshal([]byte(data), &rec)
+
+		if vars["cid"] == rec.TargetId{
+			mime = rec.Mime
+			filePath = rawPath + rec.TargetId
+		}	
+	}
+	
+	//Sets http response header (Content-type) based on MIME
+	if mime != "" {	
+		w.Header().Set("Content-Type", mime)
+		http.ServeFile(w, r, filePath)
 	}
 }
 
@@ -59,7 +113,9 @@ func displayJson(w http.ResponseWriter, r *http.Request) {
 func newRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/search", displayJson).Methods("GET")
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("../")))
+	r.HandleFunc("/raw/{cid}", serveRawFile).Methods("GET")
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("../"))) //path to be updated 
+	//r.NotFoundHandler = http.HandlerFunc(pageNotFound)
 	return r
 }
 
@@ -67,5 +123,4 @@ func newRouter() *mux.Router {
 func main() {
 	r := newRouter()	
     log.Fatal(http.ListenAndServe(":8080", r))
-
 }
